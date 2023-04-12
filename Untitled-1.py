d9a7bb17f714bb3cfc594847253f9d3794eab584
chaos <NotPKot@users.noreply.github.com>
2023-04-12 18:06:36 -0400

    na

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,7 @@
-print("hola mundo") ; 
-###este es un comentario
+
+name = input("¿cual es tu nombre?\n")
+edad= input("¿cual es tu edad?\n")
+total_edad = int(edad) + 20
+formato = f"hola, mi nombre es ", name, "tengo", str(edad), "y en 20 años tendre", str(total_edad)
+
+print(formato)
