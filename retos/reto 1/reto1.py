01dcf8da33afadbfe31e471f2bf0e9758bbc401b
chaos <NotPKot@users.noreply.github.com>
2023-06-28 20:40:01 -0400

    estesieselfinal

---
+++ retos/reto 1/reto1.py
@@ -0,0 +1,5 @@
+Nombre = input("¿cual es tu nombre?: ")
+Edad = input("¿cual es tu edad?: ")
+EdadProxima = 20 + int(Edad)
+
+print("mi nombre es",Nombre, "tengo", Edad, "años, y en 20 años tendre", EdadProxima)
